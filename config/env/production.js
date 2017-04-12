b0e1618507139fc4da0178996eeaafcd766ce230
Nguyen Hong Son <nguyenhongson.9a4@gmail.com>
2017-04-12 10:26:53 +0700

    add db cloud mlab

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@ var port = 8000;
 
 module.exports = {
     port: process.env.PORT || port,
-    db: 'mongodb://localhost/motocars',
+    db: 'mongodb://localhost/motocars' || 'mongodb://sonnh58:123456789@ds157320.mlab.com:57320/motocars',
     onesignal_app_api_key: 'ZGVhZDQ0MTUtNDYxYi00N2I2LWI0MDItMDI3N2EzZTYxZDVl',
     onesignal_api_create_message: 'https://onesignal.com/api/v1/notifications',
     time_request_inteval : 1000*60*10, //miliseconds
